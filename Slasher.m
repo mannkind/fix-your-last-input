78751c2a6785d516dc77d129c6de280a5a591ef7
Henrik Nyh <henrik@nyh.se>
2007-04-20 08:28:02 +0000

    Tidywork.
    git-svn-id: http://svn.nyh.se/cocoa/Slasher@70 c0f7b1a3-852e-0410-8f7d-8c1fbf64d33c

--- Slasher.m
+++ Slasher.m
@@ -23,20 +23,28 @@
 
 @implementation Slasher
 
+
 - (void)installPlugin {
+
 	NSLog(@"Slasher plugin loaded!");
+
 	lastOutgoingMessages = [[NSMutableDictionary alloc] init];
 	correctionComing = NO;
+
 	[[adium notificationCenter] addObserver:self selector:@selector(adiumSentOrReceivedContent:) name:Content_ContentObjectAdded object:nil];
 }
 
+
 - (void)uninstallPlugin {
+
 	[lastOutgoingMessages release];
+
 	[[adium notificationCenter] removeObserver:self name:Content_ContentObjectAdded object:nil];
 }
 
 
-//Content was sent or recieved
+
+// Content was sent or recieved
 - (void)adiumSentOrReceivedContent:(NSNotification *)notification {
 
 	// Bail if the message is a correction
@@ -56,18 +64,18 @@
 	AIAccount *account = [chat account];
 	NSAttributedString *messageString = [message messageString];
 	
-	NSAttributedString *lastMessageString = [lastOutgoingMessages valueForKey:[destination UID]];
-	
 	// Bail unless it's outgoing
 	if (![message isOutgoing]) return;
 
-	// Bail if the message wasn't written now (is old)
+	// Bail if the message wasn't written just now
 	// Casting from NSTimeInterval==double to int
-	int writtenSecondsAgo = [[NSDate date] timeIntervalSinceDate:[message date]];
-	if (writtenSecondsAgo != 0) return;
+	NSTimeInterval writtenSecondsAgo = [[NSDate date] timeIntervalSinceDate:[message date]];
+	if ((int)writtenSecondsAgo != 0) return;
 
 	// Naive way of determining if it's a transform message
 	BOOL isATransform = [messageString hasPrefix:@"s/"] && ([[messageString componentsSeparatedByString:@"/"] count] == 4) && ([[messageString componentsSeparatedByString:@"\n"] count] == 1);
+	
+	NSAttributedString *lastMessageString = [lastOutgoingMessages valueForKey:[destination UID]];
 
 	// Bail if last message wasn't a transform, or there is no history
 	if (!isATransform || !lastMessageString) {
@@ -77,7 +85,7 @@
 	
 	NSString *transformedMessage = [self string:lastMessageString withSubstitution:messageString];
 	
-	// An error occurred - so bail
+	// Bail if an error occurred in Perl
 	if ([transformedMessage length] == 0) {
 	
 		[[adium interfaceController] handleMessage:AILocalizedString(@"Invalid Substitution Error", nil)
@@ -85,9 +93,13 @@
 									withWindowTitle:@""];
 		return;
 	}
-
+	
+	// Compose new message
+	
+	// Set text
 	NSAttributedString *newMessageText = [[NSAttributedString alloc] initWithString:[AILocalizedString(@"Correction: ", nil) stringByAppendingString:transformedMessage]];
 
+	// Create message proper
 	AIContentMessage *newMessage = [[AIContentMessage alloc] initWithChat:chat source:source destination:destination date:[NSDate date] message:newMessageText];
 
 
@@ -100,8 +112,8 @@
 									withDescription:[NSString stringWithFormat:AILocalizedString(@"Unable to send message to %@.", nil), [destination displayName]]
 									withWindowTitle:@""];
 	} else {
-		// Delivered correction
-		correctionComing = YES;
+		// We delivered a correction
+		correctionComing = YES;  // Let's not track it in our substitution history
 	}
 
 	// Uncomment to crash :p
@@ -111,7 +123,7 @@
 
 }
 
-
+// Applies s/// substitution to string
 - (NSString *)string: (NSString *)string withSubstitution:(NSString*)substitution {
 
 	NSString *perlOneLiner = [[@"($s=<>)=~" stringByAppendingString:substitution] stringByAppendingString:@"; print $s;"];
