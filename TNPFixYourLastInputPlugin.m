7a3d499b17e62104d748ebfd96168eaf3287abf8
Dustin Brewer <mannkind@gmail.com>
2010-02-21 23:03:24 -0800

    Attempting to fix the ability to run shell commands (now with corrected backslash escaping!)

--- TNPFixYourLastInputPlugin.m
+++ TNPFixYourLastInputPlugin.m
@@ -206,7 +206,7 @@
 	string = [string stringByReplacingOccurrencesOfString:@"$" withString:@"\\$"];
 	string = [string stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
 	string = [string stringByReplacingOccurrencesOfString:@"`" withString:@"\\`"];
-	string = [string stringByReplacingOccurrencesOfString:@"\\" withString:@"\\"];
+	string = [string stringByReplacingOccurrencesOfString:@"\\" withString:@"\\\\"];
 	
 	return string;
 }
