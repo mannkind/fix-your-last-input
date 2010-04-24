be58a3cd1e57ad62569ce8c656e7ea4937065d3a
Ben Boeckel <MathStuf@gmail.com>
2010-04-24 12:39:03 +0800

    Escape exclamation points
    Security vulnurability:
    
    If a command such as
    
        rm "; Xdialog; echo "
    
    is in the history of the shell, then the original string consisting of
    
        !rm
    
    would break from the echo since the shell interprets `!foo` as "find the
    last command starting with 'foo' and insert here".

--- TNPFixYourLastInputPlugin.m
+++ TNPFixYourLastInputPlugin.m
@@ -208,6 +208,7 @@
 	string = [string stringByReplacingOccurrencesOfString:@"$" withString:@"\\$"];
 	string = [string stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
 	string = [string stringByReplacingOccurrencesOfString:@"`" withString:@"\\`"];
+	string = [string stringByReplacingOccurrencesOfString:@"!" withString:@"\\!"];
 	
 	return string;
 }
